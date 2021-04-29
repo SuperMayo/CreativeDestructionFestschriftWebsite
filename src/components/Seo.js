1f07a2437351fe1be8a886aefa12c3cf15a6ab0a
Antoine Mayerowitz <a.mayerowitz@gmail.com>
2021-04-29 10:02:17 +0200

    update Seo
    Fix twitter card
    
    Add trailing slash to og:url

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -7,18 +7,20 @@ const Seo = ({ lang, meta, title }) => {
         <Helmet 
             htmlAttributes={{lang, }}
             title={title} 
-            meta={[ 
-                { name: `description`, content: meta.description}, 
-                { property: `og:title`, content: title}, 
-                { property: `og:description`, content: meta.description}, 
-                { property: `og:type`, content: `website`},
-                { property: `og:image`, content: meta.image},
-                { property: `og:url`, content: `https://creativedestruction2021.org`},
-                { name: `twitter:card`, content: meta.card},
-                { name: `twitter:title`, content: title},
-                { name: `twitter:description`, content: meta.description},
-                { name: `twitter:image`, content: meta.image},
-                { name: `twitter:site`, content: meta.twitterSite},].concat(meta)}/>
+        >
+        <meta name="description" content= {meta.description} />
+        <meta property="og:title" content= {title} /> 
+        <meta property="og:description" content= {meta.description} /> 
+        <meta property="og:type" content= "website" /> 
+        <meta property="og:image" content= {meta.image} /> 
+        <meta property="og:url" content= "https://creativedestruction2021.org/" />
+        <meta name= "twitter:card" content= {meta.card} />
+        <meta name= "twitter:title" content= {title} />
+        <meta name= "twitter:description" content= {meta.description} />
+        <meta name= "twitter:image" content= {meta.image} />
+        <meta name= "twitter:site" content= {meta.twitterSite} />
+        
+        </Helmet>
     )
 }
 
